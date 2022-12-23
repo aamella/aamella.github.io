84166b7360aeeb8198996edbd792f2ffb1aec77a
Anthony Amella <aamella@cps.edu>
2022-12-22 22:04:04 -0600

    initial

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -55,14 +55,7 @@ export const head: HeadConfig = {}
 
 export const header: HeaderConfig = {
   nav: [
-    {
-      text: 'Get Started',
-      link: '/hello-world'
-    },
-    {
-      text: 'Elements',
-      link: '/hello-world/elements'
-    }
+    
   ]
 }
 
--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,14 +3,14 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
+  title: 'Anthony\'s Blog',
   subtitle: 'Sweet & Powerful SvelteKit Blog Template',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    name: 'John Doe',
+    name: 'Anthony Amella',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'ECE@UIUC'
   },
   themeColor: '#3D4451'
 }
